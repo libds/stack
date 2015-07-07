58d0942f9d9c9f3ee7a4fa3a6be6f3be1d6ef4cc
Khalid Lafi <lafi.khalid@gmail.com>
2015-07-07 03:41:54 +0300

    Add API docs

--- stack.go
+++ stack.go
@@ -14,10 +14,12 @@ func New() *Stack {
 	return &Stack{}
 }
 
+// Push down an item to the stack
 func (s *Stack) Push(v string) {
 	s.items = append([]string{v}, s.items...)
 }
 
+// Return and remove the first item in the list
 func (s *Stack) Pop() (string, error) {
 	if len(s.items) == 0 {
 		return "", StackEmpty
@@ -27,14 +29,17 @@ func (s *Stack) Pop() (string, error) {
 	return v, nil
 }
 
+// Returns true if stack is empty
 func (s *Stack) Empty() bool {
 	return len(s.items) == 0
 }
 
+// Returns the amount of items currently in the stack
 func (s *Stack) Size() int {
 	return len(s.items)
 }
 
+// Get the underlying array of items
 func (s *Stack) Arr() []string {
 	return s.items
 }
